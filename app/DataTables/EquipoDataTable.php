ccba421d159ab971efc234a693f280346627d17d
Fabian1992 <fabi.lopez1992@gmail.com>
2019-05-08 12:18:12 -0500

    Inicio de equipo

---
+++ app/DataTables/EquipoDataTable.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace ioliga\DataTables;
+
+use ioliga\Models\Equipo\Equipo;
+use Yajra\DataTables\Services\DataTable;
+
+class EquipoDataTable extends DataTable
+{
+    /**
+     * Build DataTable class.
+     *
+     * @param mixed $query Results from query() method.
+     * @return \Yajra\DataTables\DataTableAbstract
+     */
+    public function dataTable($query)
+    {
+        return datatables($query)
+            ->addColumn('action', 'equipo.action');
+    }
+
+    /**
+     * Get query source of dataTable.
+     *
+     * @param \ioliga\Models\Equipo\Equipo $model
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function query(Equipo $model)
+    {
+        return $model->newQuery()->select($this->getColumns());
+    }
+
+    /**
+     * Optional method if you want to use html builder.
+     *
+     * @return \Yajra\DataTables\Html\Builder
+     */
+    public function html()
+    {
+        return $this->builder()
+                    ->columns($this->getColumns())
+                    ->minifiedAjax()
+                    ->addAction(['width' => '80px'])
+                    ->parameters($this->getBuilderParameters());
+    }
+
+    /**
+     * Get columns.
+     *
+     * @return array
+     */
+    protected function getColumns()
+    {
+        return [
+            'id',
+            'nombre',        
+            'localidad',
+            'telefono',
+            'anioCreacion',
+            'fraseIdentificacion',
+            'color',
+            'color1',
+            'color2',
+            'foto',
+            'estado'
+        ];
+    }
+
+    /**
+     * Get filename for export.
+     *
+     * @return string
+     */
+    protected function filename()
+    {
+        return 'Equipo_' . date('YmdHis');
+    }
+}
